c9ac23ae0319caaff1db74af6df5803721f7f23b
Joseph Vargas <joseph@cpokemon.com>
2019-06-16 02:00:19 -0600

    errorParser more fixes

--- pages/mainscreen.vue
+++ pages/mainscreen.vue
@@ -11,7 +11,7 @@
               center
               rounded="circle"
               alt="Circle image"
-              class="mu-2"
+              class="mt-2"
             >
             </b-img>
             <b-container class="ml-3 mr-3">
@@ -50,7 +50,7 @@
             <b-container>
               <div>
                 <h2 class="title mb-2 mt-2 ">
-                  Próximos Cuidados
+                  Próximos cuidados
                 </h2>
 
                 <b-card
@@ -97,15 +97,29 @@
                         </h5>
                       </b-form-row>
                     </b-col>
+
                   </b-row>
                 </b-card>
 
-                <b-card
+                <b-button
+                  to="/"
+                  variant="primary"
+                  class="btn btn-primary btn-lg btn-block mt-2 mb-2"
+                  >Ver más</b-button
+                >
+              </div>
+            </b-container>
+          </b-row>
+          <b-row class="border rounded mb-5">
+            <h2 class="title mb-2 mt-2 ml-2 ">
+                  Tareas pendientes
+                </h2>
+              <b-card
                   no-body
                   class="overflow-hidden mt-1"
                   style="max-width: 540px;"
                 >
-                  <b-row no-gutters mb="6" style="height:100px">
+                  <b-row no-gutters style="height:100px">
                     <b-col md="3">
                       <b-card
                         border-variant="primary"
@@ -115,21 +129,38 @@
                         <b-card-text>Lorem ipsum dolor s.</b-card-text>
                       </b-card>
                     </b-col>
-                    <b-col md="6">
-                      <b-card-body title="Horizontal Card">
-                        <b-card-text>
-                          This is a wider card with supporting text as a natural
-                          lead-in to additional content.
-                        </b-card-text>
-                      </b-card-body>
+                    <b-col cols="2">
+                      <b-row class="mb-2 mt-4">
+                        <fa icon="map-marker-alt" class="mr-2 mt-1"></fa>Linea1
+                      </b-row>
+                      <b-row class="mb-2">
+                        <fa icon="clock" class="mr-1 mt-1"></fa>Linea2
+                      </b-row>
                     </b-col>
+                    <b-col cols="2">
+                      <b-img
+                        src="https://picsum.photos/250/250/?image=54"
+                        center
+                        rounded="circle"
+                        alt="Circle image"
+                        class="mu-1 mt-1"
+                        style="width:90px; height:90px"
+                      >
+                      </b-img>
+                    </b-col>
+                    <b-col class="ml-4">
+                      <b-form-row class=" mt-3 ">
+                        <h3 class="title">
+                          Joseph PICHAS
+                        </h3>
+                        <h5 class="text-muted ml-2">
+                          nombre del animalejo
+                        </h5>
+                      </b-form-row>
+                    </b-col>
+
                   </b-row>
                 </b-card>
-              </div>
-            </b-container>
-          </b-row>
-          <b-row class="border rounded mb-5">
-            Reportes
           </b-row>
         </b-col>
       </b-row>
--- plugins/errorParser.js
+++ plugins/errorParser.js
@@ -1,25 +1,14 @@
-import has from "lodash";
+import get from "lodash/get";
 import Vue from "vue";
 
 Vue.mixin({
   methods: {
     errorParser(error) {
-      if (has(error, "response")) {
-        if (has(error.response, "data")) {
-          if (has(error.response.data, "error")) {
-            return error.response.data.error;
-          } else {
-            console.error("DigiPet Error R001");
-            return "Ha ocurrido un error. Por favor inténtelo de nuevo.";
-          }
-        } else {
-          console.error("DigiPet Error R002");
-          return "Ha ocurrido un error. Por favor inténtelo de nuevo.";
-        }
-      } else {
-        console.error("DigiPet Error R003");
-        return "Ha ocurrido un error. Por favor inténtelo de nuevo.";
-      }
+      return get(
+        error,
+        "response.data.error",
+        "Ha ocurrido un error. Por favor inténtelo de nuevo"
+      );
     }
   }
 });
